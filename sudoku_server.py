9613f85fad77310a73ed256d7e1fe30b328a1fba
sourcewire <cyberskis@proton.me>
2024-02-23 14:51:05 -0800

    adding comments

--- sudoku_server.py
+++ sudoku_server.py
@@ -10,28 +10,33 @@ print(type(python_sudoku_arr))
 
 
 
-def sudoku_verifier(python_sudoku_arr):
-    #
+def sudoku_verifier(sudoku_grid):
+    #creates lists of 9 empty sets
     rows = [set() for _ in range(9)]
     cols = [set() for _ in range(9)]
     subgrids = [set() for _ in range(9)]
 
+    #loop over grid
     for i in range(9):
         for j in range(9):
-            num = python_sudoku_arr[i][j]
+            #get num in current cell
+            num = sudoku_grid[i][j]
             if num == 0:
                 continue
-
+            
+            #calculate the index of the 3x3 subgrid where the current cell is located
             subgrid_index = (i // 3) * 3 + j // 3
-
+            
+            #if current num is already in the grid, return false
             if num in rows[i] or num in cols[j] or num in subgrids[subgrid_index]:
-                return False
+                return 0
 
+            #add the num
             rows[i].add(num)
             cols[j].add(num)
             subgrids[subgrid_index].add(num)
 
-    return True
+    return 1
 
 
 if sudoku_verifier(python_sudoku_arr):
